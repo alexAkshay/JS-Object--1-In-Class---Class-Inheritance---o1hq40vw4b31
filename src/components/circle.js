91ded14b9462462a5b107bf58427a70b27ab8cf9
ALEX <akshayhumane@outlook.com>
2021-11-30 22:52:04 +0530

    FInal Commit

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,4 +4,18 @@
 
 
 // export class using module.exports
+
+let Shape = require('./shape')
+
+
+export class Circle extends Shape {
+    constructor(color) {
+        super(color);
+    }
+    calculateArea() {
+        console.log(`${this.color}  area is calculated`)
+
+    }
+}
+
 module.exports = Circle
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
+
+    drawShape() {
+        console.log(`${this.color} is colored`);
+
+    }
+    calculateArea() {
+        console.log(`${this.color}  area is to be calculated`)
+    }
+
+}
+module.exports = Shape
